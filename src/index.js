97c38a60c10d831b56a5827acaddc7804e80fa24
Sergio Yonashiro <sergio.yonashiro@outlook.com>
2019-01-12 10:37:49 -0200

    Instala Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './assets/css/bootstrap.scss'
+import './assets/js/bootstrap'
+
 ReactDOM.render(
     <h1>Tiny Blog</h1>,
     document.getElementById('root')
